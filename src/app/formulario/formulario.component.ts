73c54b2cbf980ed49ea1378a8c99b8174d0b05f1
dtaubaso <d.taubaso@gmail.com>
2023-01-15 19:41:05 -0300

    componente formulario

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  tipo:string="ingresoOperacion";
+  descripcionInput:string;
+  valorInput:number;
+  
+  constructor(private ingresoServicio:IngresoServicio,
+    private egresoServicio:EgresoServicio){}
+
+  tipoOperacion(evento:Event){
+    this.tipo = (evento.target as HTMLInputElement).value;
+  }
+
+  agregarValor(){
+    if(this.tipo === "ingresoOperacion"){
+      this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput, this.valorInput))
+    }
+    else{
+      this.egresoServicio.egresos.push(new Egreso(this.descripcionInput, this.valorInput))
+    }
+
+  }
 
 }
