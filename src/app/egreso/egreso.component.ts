1ffd154091c4386c05b775c11550d84eadaf1f10
dtaubaso <d.taubaso@gmail.com>
2023-01-15 18:43:09 -0300

    componentes egresos

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
 
 @Component({
   selector: 'app-egreso',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./egreso.component.css']
 })
 export class EgresoComponent {
+
+  egresos:Egreso[]=[];
+  @Input() ingresoTotal:number;
+
+  constructor(private egresoServicio:EgresoServicio){}
+
+  ngOnInit(){
+    this.egresos = this.egresoServicio.egresos;
+  }
+
+  eliminarEgreso(egreso:Egreso){
+    this.egresoServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso:Egreso){
+    return egreso.valor/this.ingresoTotal;
+  }
 
 }
--- src/app/egreso/egreso.servicio.ts
+++ src/app/egreso/egreso.servicio.ts
@@ -4,5 +4,10 @@ export class EgresoServicio{
     egresos:Egreso[] = [
         new Egreso("impuestos", 2300),
         new Egreso("Comida", 800)
-    ]
+    ];
+
+    eliminar(egreso:Egreso){
+        const indice: number = this.egresos.indexOf(egreso);
+        this.egresos.splice(indice,1);
+    }
 }
